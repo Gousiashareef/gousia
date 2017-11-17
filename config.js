ac8012c2170fb9a98c5570eda970a6dc99d7c05b
Gousiashareef <gousiashareef3@gmail.com>
2017-11-17 19:06:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/live_stream'
+    'mongoUrl' : 'mongodb://www.colourssoftware.com:27017/stream'
 }
